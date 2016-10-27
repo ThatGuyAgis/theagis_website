db21f3f64ad8fec11e4a4eb8cbe26b6dfe17f753
Kacper Kubicki <agisprivate@gmail.com>
2016-10-27 11:21:44 +0200

    improved secret things

--- js/important/eggs/testFunction.js
+++ js/important/eggs/testFunction.js
@@ -1,3 +1,14 @@
+/*
+
+  Here, add a small endless-runner game similar to Google's Dino, but with
+  the channel esthetic and "memes"
+
+  It will make for some fun, later on add it to the error page.
+
+*/
+
+
+
 function testFunction() {
   document.getElementById("test_string").innerHTML = "Easter Egg discovered!  ";
 }
